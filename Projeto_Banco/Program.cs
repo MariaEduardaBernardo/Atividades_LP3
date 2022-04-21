9cb15cfeaf0916e992b8e00c31ad8f0607412839
Maria Eduarda <mariaedu9434@gmail.com>
2022-04-21 13:14:56 -0300

    Dados da conta

--- Projeto_Banco/Program.cs
+++ Projeto_Banco/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Conta c1 = new Conta(1);
+Conta c2 = new Conta(2);
+
+Console.WriteLine("\n--- Conta 1 ---");
+c1.Despositar(200.00);
+c1.Sacar(100.00);
+Console.WriteLine("Saldo atual: " +c1.Saldo);
+
+Console.WriteLine("\n--- Conta 2 ---");
+c2.Despositar(100.00);
+c2.Sacar(45.00);
+Console.WriteLine("Saldo atual: " +c2.Saldo);
