d5838c62557e7ee5c727ef629ae5c1f4717fe271
Maria Eduarda <mariaedu9434@gmail.com>
2022-04-21 13:34:20 -0300

    Classe Trapezio

---
+++ Prova_1-Bimestre/Trapezio.cs
@@ -0,0 +1,20 @@
+public class Trapezio
+{
+    public double B1 { get; set; }
+    public double B2 { get; set; }
+    public double L1 { get; set; }
+    public double L2 { get; set; }
+    public double Altura { get; set; }
+    public double Area { get => ((B1+B2)*Altura)/2;}
+    public double Perimetro { get => B1+B2+L1+L2;}
+
+    public string AreaPerimetro{ get => "Trapezio: area "+Area+", perimetro "+Perimetro;}
+    public Trapezio(double b1,double b2,double l1,double l2,double altura)
+    {
+        B1 = b1;
+        B1 = b2;
+        B1 = l1;
+        B1 = l2;
+        B1 = altura;
+    }
+}
