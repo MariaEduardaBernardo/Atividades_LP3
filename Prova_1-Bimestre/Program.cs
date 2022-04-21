1ffdb332e7682190a2c1f0e6fd6c42648185666b
Maria Eduarda <mariaedu9434@gmail.com>
2022-04-21 13:55:29 -0300

    Apresentar area e perimetro do trapezio

--- Prova_1-Bimestre/Program.cs
+++ Prova_1-Bimestre/Program.cs
@@ -1,17 +1,18 @@
-﻿Console.WriteLine("Digite a base maior do trapezio: ");
+﻿Console.WriteLine("Entre com a base maior");
 double b1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Digite a base menor do trapezio: ");
+Console.WriteLine("Entre com a base menor");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Digite o lado 1 do trapezio: ");
+Console.WriteLine("Entre com o lado 1");
 double l1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Digite o lado 2 do trapezio: ");
+Console.WriteLine("Entre com o lado 2");
 double l2 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Digite a altura do trapezio: ");
+Console.WriteLine("Entre com a altura");
 double altura = Convert.ToDouble(Console.ReadLine());
 
 
 Trapezio t1 = new Trapezio(b1, b2, l1, l2, altura);
+Console.WriteLine(t1.AreaPerimetro);
--- Prova_1-Bimestre/Trapezio.cs
+++ Prova_1-Bimestre/Trapezio.cs
@@ -5,16 +5,16 @@ public class Trapezio
     public double L1 { get; set; }
     public double L2 { get; set; }
     public double Altura { get; set; }
-    public double Area { get => ((B1+B2)*Altura)/2;}
-    public double Perimetro { get => B1+B2+L1+L2;}
+    public double Area { get => ((B1+B2)*Altura)/2; }
+    public double Perimetro { get => B1+B2+L1+L2; }
 
-    public string AreaPerimetro{ get => "Trapezio: area "+Area+", perimetro "+Perimetro;}
+    public string AreaPerimetro{ get => "Trapezio: area " +Area+ ", perimetro "+Perimetro;}
     public Trapezio(double b1,double b2,double l1,double l2,double altura)
     {
         B1 = b1;
-        B1 = b2;
-        B1 = l1;
-        B1 = l2;
-        B1 = altura;
+        B2 = b2;
+        L1 = l1;
+        L2 = l2;
+        Altura = altura;
     }
 }
