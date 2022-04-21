70aeee9b48ba1bdabb1b021e698e80f2f099f6ea
Maria Eduarda <mariaedu9434@gmail.com>
2022-04-21 13:40:20 -0300

    Entrada de dados do trapezio

--- Prova_1-Bimestre/Program.cs
+++ Prova_1-Bimestre/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite a base maior do trapezio: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a base menor do trapezio: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o lado 1 do trapezio: ");
+double l1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o lado 2 do trapezio: ");
+double l2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a altura do trapezio: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+
+Trapezio t1 = new Trapezio(b1, b2, l1, l2, altura);
