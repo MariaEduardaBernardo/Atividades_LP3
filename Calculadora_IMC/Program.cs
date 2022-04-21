840274479e58d71ce9ad2c40cf169ceeba955d5b
Maria Eduarda <mariaedu9434@gmail.com>
2022-04-21 12:24:36 -0300

    Estrutura if - else

--- Calculadora_IMC/Program.cs
+++ Calculadora_IMC/Program.cs
@@ -15,5 +15,40 @@
 
         Console.WriteLine("{0}, sua altura é {1} e seu peso é {2}", nome, altura, peso);
         Console.WriteLine("Seu IMC é: {0}", imc);
+
+        if (imc < 17)
+        {
+            Console.WriteLine("Muito abaixo do peso");
+        }else
+        
+        if ((imc >= 17) && (imc < 18.49))
+        {
+            Console.WriteLine("Abaixo do peso");
+        }else
+
+        if ((imc >= 18.5) && (imc < 25))
+        {
+            Console.WriteLine("Peso normal");
+        }else
+
+        if ((imc >= 25) && (imc < 30))
+        {
+            Console.WriteLine("Acima do peso");
+        }else
+
+        if ((imc >= 30) && (imc < 35))
+        {
+            Console.WriteLine("Obesidade I");
+        }else
+
+        if ((imc >= 35) && (imc < 40))
+        {
+            Console.WriteLine("Obesidade II (Severa)");
+        }else
+
+        if (imc >= 40)
+        {
+            Console.WriteLine("Obesidade III (Mórbida)");
+        }
     }
 }
