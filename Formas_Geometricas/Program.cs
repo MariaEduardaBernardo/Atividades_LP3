cf4549ad26f35098b61896f158b59395329ba7b9
Maria Eduarda <mariaedu9434@gmail.com>
2022-04-21 12:02:13 -0300

    Dados do quadrado

--- Formas_Geometricas/Program.cs
+++ Formas_Geometricas/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Quadrado q1 = new Quadrado(10.0);
+Quadrado q2 = new Quadrado(4.5);
+
+q1.Lado = 15;
+
+Console.WriteLine("\n--- Quadrado 1 ---\n");
+Console.WriteLine("Lado: " +q1.Lado);
+Console.WriteLine("Area: " +q1.Area);
+Console.WriteLine("Perimetro: " +q1.Perimetro);
+
+Console.WriteLine("\n--- Quadrado 2 ---\n");
+Console.WriteLine("Lado: " +q2.Lado);
+Console.WriteLine("Area: " +q2.Area);
+Console.WriteLine("Perimetro: " +q2.Perimetro);
